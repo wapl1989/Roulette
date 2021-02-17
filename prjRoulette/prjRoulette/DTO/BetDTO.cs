1af37dd346c7257c553c5b567590d089a3cbebf0
WilliamPL <wapl1989@gmail.com>
2021-02-17 00:19:53 -0500

    creacion del DTO para las apuestas

---
+++ prjRoulette/prjRoulette/DTO/BetDTO.cs
@@ -0,0 +1,28 @@
+﻿using prjRoulette.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prjRoulette.DTO
+{
+    public class BetDTO
+    {
+        [Key]
+        [Required]
+        public string Id { get; set; }
+        [Range(0,36)]
+        public int NumberBet { get; set; }        
+        public BetColorEnum ColorBet { get; set; } 
+        public BetTypes TypeBet { get; set; }
+        [Range(1,10000)]
+        [Required]
+        public decimal ValueBet { get; set; }
+        [Required]
+        public string IdRoulette { get; set; }        
+        public decimal EarnedMoney { get; set; }
+        [Required]
+        public string User { get; set; }
+    }
+}
--- prjRoulette/prjRoulette/DTO/RouletteDTO.cs
+++ prjRoulette/prjRoulette/DTO/RouletteDTO.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace prjRoulette.DTO
 {
+    
     public class RouletteDTO
     {
         [Key]
